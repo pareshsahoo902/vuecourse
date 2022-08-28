2143bfec131b9faecb3051ea5e54aa05c1c9554d
Aswini Sahoo <aswini.sahoo@tibersoft.com>
2022-08-28 22:47:04 +0530

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,76 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!-- THIS set of code will hide and unhide the text -->
+<!-- <template>
+  <div>
+      <button @click='show = !show'>Show/Hide</button>
+      <span v-show="show">Click to hide this text</span>
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'toggle-button',
+   data:  function(){
+    return {
+      show: true
+    }
+   }
+}
+</script> -->
+
+<!-- 
+This set of code is to check if the age is more than 18 then enable the button
+<template>
+  <div>
+      <input v-model.number="age" type="number" placeholder="Age" />
+      <button :disabled="underAge" id="btn" onclick="alert('Buy stuff')">Buy stuff</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'conditional-disabling',
+   data: function () {
+    return {
+      age: null
+    };
+   },
+  computed: {
+    underAge: function () {
+      return this.age < 18;
+    }
   }
 }
+</script> -->
+
+<!-- This set of code is a simple example of todo app -->
+<template>
+  <div>
+      <input v-model="item" type="text" placeholder="item" />
+      <button @click="addItem">Add</button>
+      <ul>
+         <li v-for="todo in todos" :key="todo">
+           {{ todo }}
+         </li>
+      </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'to-do',
+   data: function () {
+    return {
+      todos: [],
+      item: ""
+    };
+   },
+   methods: {
+     addItem() {
+       this.todos.push(this.item);
+       this.item = "";
+     }
+   }
+}
 </script>
+
+
 
 <style>
 #app {
