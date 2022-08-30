bf2d9a089fcdfc0273d2189f8cb7a69c5216e414
Aswini Sahoo <aswini.sahoo@tibersoft.com>
2022-08-30 13:26:25 +0530

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,87 +1,14 @@
-<!-- This set of code is a simple example of todo app -->
-<!-- <template>
-  <div>
-      <h1 v-if="1==0">True statement</h1>
-      <h1 v-else-if="10>6">Else True statement</h1>
-      <h1 v-else>False statement</h1>
-
-
-
-
-      
-  </div>
-</template>
-<script>
-export default {
-  name: 'to-do',
-   data: function () {
-    return {
-      todos: [],
-      item: ""
-    };
-   },
-   methods: {
-     addItem() {
-       this.todos.push(this.item);
-       this.item = "";
-     }
-   }
-}
-</script> -->
-
-
 <template>
-  <div class="container">
-      <h3 class="p-3 text-center">Vue 3 - Display a list of items with v-for</h3>
-      <table class="table table-striped table-bordered">
-          <thead>
-              <tr>
-                  <th>Name</th>
-                  <th>Email</th>
-                  <th>Role</th>
-              </tr>
-          </thead>
-          <tbody>
-              <tr v-for="user in users" :key="user.id">
-                  <td>{{user.firstName}} {{user.lastName}}</td>
-                  <td>{{user.email}}</td>
-                  <td>{{user.role}}</td>
-              </tr>
-          </tbody>
-      </table>
-  </div>    
+  <Home />
 </template>
 
 <script>
-import { ref } from 'vue';
-
+import Home from './components/Home.vue';
 export default {
   name: 'App',
-  setup() {
-      // make users variable reactive with the ref() function
-      const users = ref([
-          { firstName: 'Frank', lastName: 'Murphy', email: 'frank.murphy@test.com', role: 'User' },
-          { firstName: 'Vic', lastName: 'Reynolds', email: 'vic.reynolds@test.com', role: 'Admin' },
-          { firstName: 'Gina', lastName: 'Jabowski', email: 'gina.jabowski@test.com', role: 'Admin' },
-          { firstName: 'Jessi', lastName: 'Glaser', email: 'jessi.glaser@test.com', role: 'User' },
-          { firstName: 'Jay', lastName: 'Bilzerian', email: 'jay.bilzerian@test.com', role: 'User' }
-      ]);
-
-      return {
-          users
-      };
+  components: {
+    Home
   }
 }
 </script>
 
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,50 @@
+<!-- DISPLAY NORMAL PROP -->
+<!-- 
+    <template>
+    <div>
+        <h1>Template in Vue.js</h1>
+        <h1>{{name}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Home-Vue",
+    data(){
+        return{
+            name:"JOHN DOE"
+        }
+        
+    }
+};
+</script> -->
+
+
+<!-- MAKE BUTTON CLICK -->
+
+<template>
+    <div>
+        <h1>Template in Vue.js</h1>
+        <h1>{{name}}</h1>
+        <button v-on:click="greet">Greet</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Home-Vue",
+    data(){
+        return{
+            name:"JOHN DOE"
+        }
+        
+    },
+    methods:{
+        greet()
+        {
+            alert("Hello "+this.name);
+            this.name = this.name.split(" ")[0]
+        }
+    }
+};
+</script>
