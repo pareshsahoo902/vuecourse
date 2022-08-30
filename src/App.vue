e7f633872878608a287b2eddb0c8ec8a20f915d6
Aswini Sahoo <aswini.sahoo@tibersoft.com>
2022-08-30 15:48:01 +0530

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,36 @@
 <template>
-  <Home />
+  <div>
+  <Home v-bind:title="title" />
+  
+  <button @click="showPopup = true">Open Popup</button>
+
+  <Popup v-show="showPopup" 
+  @submit="submitPopup" 
+  @closePopup="showPopup = false"
+  />
+</div>
 </template>
 
 <script>
 import Home from './components/Home.vue';
+import Popup from './components/Popup.vue';
 export default {
   name: 'App',
   components: {
-    Home
+    Home,
+    Popup
+},
+  data(){
+    return{
+      title:"MainPage",
+      showPopup:false
+    }
+  },
+  methods:{
+    submitPopup:function(text){
+      this.title = text;
+      this.showPopup = false;
+    }
   }
 }
 </script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,49 +1,16 @@
-<!-- DISPLAY NORMAL PROP -->
-<!-- 
-    <template>
-    <div>
-        <h1>Template in Vue.js</h1>
-        <h1>{{name}}</h1>
-    </div>
-</template>
-
-<script>
-export default {
-    name:"Home-Vue",
-    data(){
-        return{
-            name:"JOHN DOE"
-        }
-        
-    }
-};
-</script> -->
-
-
-<!-- MAKE BUTTON CLICK -->
-
 <template>
     <div>
-        <h1>Template in Vue.js</h1>
-        <h1>{{name}}</h1>
-        <button v-on:click="greet">Greet</button>
+        <h1 v-on:click="changeTitle">{{title}}</h1>
+        
     </div>
 </template>
 
 <script>
 export default {
     name:"Home-Vue",
-    data(){
-        return{
-            name:"JOHN DOE"
-        }
-        
-    },
-    methods:{
-        greet()
-        {
-            alert("Hello "+this.name);
-            this.name = this.name.split(" ")[0]
+    props:{
+        title:{
+            type:String
         }
     }
 };
