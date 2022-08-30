d5403073a2e205a855caa2ae4be4d59a4a6ed61f
Aswini Sahoo <aswini.sahoo@tibersoft.com>
2022-08-30 16:19:36 +0530

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,18 @@
 <!-- If the volume reaches 16 give a warning -->
 <template>
   <div>
-    <h2>Volume Tracker (0 - 20)</h2>
-    <h3>Current Volume: {{volume}}</h3>
-    <button @click="volume = volume + 2" >Increase</button>
-    <button  @click="volume = volume - 2" >Decrease</button>
+    <Greet name="John" />
+    <Greet name="Emily" />
+    <Greet name="James" />
 </div>
 </template>
 
 <script>
+import Greet from './components/Greet.vue';
 
 export default {
-  name: 'App',
-  data(){
-    return{
-      volume : 0,
-    }
-  },
-  watch:{
-    volume(newVal,oldVal){
-      newVal == 16  && newVal > oldVal?alert('High Volume Warning!'):null
-    }
-  }
+    name: "App",
+    components: { Greet }
 }
 </script>
 
---
+++ src/components/Greet.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2>Good morning! :{{name}}</h2>
+</template>
+
+<script>
+export default {
+    name:'Greet-Page',
+    props:['name']
+}
+</script>
+
+<style>
+
+</style>
